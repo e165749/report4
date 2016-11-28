101da933e16f5efed9537c91b7bcc54e69a47f55
Mitsuki ESU <e165749@ie.u-ryukyu.ac.jp>
2016-11-28 21:58:02 +0900

    prog2 report4

--- src/main/jp/ac/uryukyu/ie/e165749/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165749/Enemy.java
@@ -1,4 +1,4 @@
-package jp.ac.uryukyu.ie.e165749;
+package jp.ac.uryukyu.ie. e165749;
 
 /**
  * 敵クラス。
@@ -13,6 +13,7 @@ public class Enemy {
     int hitPoint;
     int attack;
     boolean dead;
+
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
      * @param name モンスター名
@@ -26,6 +27,7 @@ public class Enemy {
         dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
+
     /**
      * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
      * @return boolean
@@ -33,21 +35,22 @@ public class Enemy {
     public boolean isDead() {
         return dead;
     }
+
     public String getName(){
         return name;
     }
+
     /**
      * Heroへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        if(hitPoint == 0) {
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
+        int damage = (int)(Math.random() * attack);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+        hero.wounded(damage);
     }
+
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
@@ -60,4 +63,5 @@ public class Enemy {
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
     }
+
 }
